1585ee481c9b9caa39971e92861c56923d0c8025
Fabrício <fabricio_c_anelli@hotmail.com>
2023-07-30 00:08:37 -0300

    feat: adding skeleton loading

--- src/view/components/Posts.tsx
+++ src/view/components/Posts.tsx
@@ -2,12 +2,13 @@ import { postsService } from '../../app/services/postsService';
 import { Post } from '../../app/interfaces/Post';
 import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
+import { Skeleton } from './Skeleton';
 import { PostList } from './PostList';
 
 export const Posts = () => {
   const [posts, setPosts] = useState<Post>();
 
-  const { data } = useQuery<Post>({
+  const { data, isFetching } = useQuery<Post>({
     queryKey: ['getPosts'],
     queryFn: () => postsService.getAll({ orderBy: 'desc', limit: 9 }),
     staleTime: 1000 * 60 * 5,
@@ -19,7 +20,27 @@ export const Posts = () => {
     }
   }, [data]);
 
-  return (
+  return isFetching ? (
+    <section className="bg-gray-50 sm:ml-64">
+      <div className="px-4 py-16 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="flex items-end justify-between">
+          <div className="flex-1 text-center lg:text-left">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+              Latest from blog
+            </h2>
+            <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600 lg:mx-0">
+              Keep up with the latest world news, all current affairs and
+              coverage. News of the day, photos and videos. Be the first to
+              know.
+            </p>
+          </div>
+        </div>
+        <div className="grid max-w-md grid-cols-1 gap-6 mx-auto mt-8 lg:mt-16 lg:grid-cols-3 lg:max-w-full">
+          <Skeleton count={9} />
+        </div>
+      </div>
+    </section>
+  ) : (
     <section className="bg-gray-50 sm:ml-64">
       <div className="px-4 py-16 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="flex items-end justify-between">
