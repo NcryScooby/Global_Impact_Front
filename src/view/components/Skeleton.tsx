00c2617d58ef8c5e75177182ee0622936e741e79
Fabrício <fabricio_c_anelli@hotmail.com>
2023-07-30 00:10:07 -0300

    fix: adding animate-pulse in Skeleton

--- src/view/components/Skeleton.tsx
+++ src/view/components/Skeleton.tsx
@@ -5,7 +5,7 @@ interface SkeletonProps {
 export const Skeleton = ({ count }: SkeletonProps) => {
   return Array.from({ length: count || 3 }).map((_, index) => (
     <>
-      <div key={index} role="status" className="sm: p-5">
+      <div key={index} role="status" className="sm: p-5 animate-pulse">
         <div className="flex items-center justify-center h-52 mb-4 bg-gray-300 dark:bg-gray-300"></div>
         <div className="h-2 bg-gray-200 rounded-sm dark:bg-gray-300 w-40 mb-4 mt-6"></div>
         <div className="h-2 bg-gray-200 rounded-sm dark:bg-gray-300 mb-2.5 w-44"></div>
