ea77392fdefc76b8ecad1d434ebabd13f4be46d9
Fabrício <fabricio_c_anelli@hotmail.com>
2023-09-07 15:38:05 -0300

    feat: adding POSTS_TITLE_MAP constant

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -21,3 +21,10 @@ export const FILTER_OPTIONS = [
   { id: 'popularity', name: 'Most Popular' },
   { id: 'views', name: 'Most Viewed' },
 ];
+
+export const POSTS_TITLE_MAP = {
+  [FILTER_OPTIONS[0].id]: 'Latest from blog',
+  [FILTER_OPTIONS[1].id]: 'Oldest from blog',
+  [FILTER_OPTIONS[2].id]: 'Most popular posts',
+  [FILTER_OPTIONS[3].id]: 'Most viewed posts'
+};
